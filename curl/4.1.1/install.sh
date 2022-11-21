a5868cf40f49ca31b499bcca14ba177448dc2c58
maheshopsmx <mahesh.kota@opsmx.io>
2022-11-21 17:46:14 +0530

    haldeled mac condition

--- curl/4.1.1/install.sh
+++ curl/4.1.1/install.sh
@@ -131,9 +131,13 @@ if [ $? == 0 ];
 then
   echo "yq present in server.."
 else
-  sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 > /dev/null 2>&1
-  sudo chmod a+x /usr/local/bin/yq
-  echo "Installed yq dependency"
+   if [[ $OSTYPE == 'darwin'* ]]; then
+       brew install yq
+   else
+       sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 > /dev/null 2>&1
+       sudo chmod a+x /usr/local/bin/yq
+       echo "Installed yq dependency"
+   fi
 fi
 }
 
@@ -145,13 +149,13 @@ curl -o values.yaml https://raw.githubusercontent.com/OpsMx/enterprise-argo/main
 
 
 isdmodevalues(){
-sed -i "s/oes.example.ops.com/$isduiurl/g" values.yaml
-if [ $? == 0 ];
-then
-   echo ""
-else
+
+if [[ $OSTYPE == 'darwin'* ]]; then
    sed -i.bu "s/oes.example.ops.com/$isduiurl/g" values.yaml
+else
+   sed -i "s/oes.example.ops.com/$isduiurl/g" values.yaml
 fi
+
 yq e -i '.installArgoCD = false' values.yaml
 yq e -i '.installArgoRollouts = false' values.yaml
 yq e -i '.installArgoEvents = false' values.yaml
@@ -162,19 +166,16 @@ yq e -i '.autoconfigureagent = false' values.yaml
 }
 
 isdargomodevalues(){
-sed -i "s/oes.example.ops.com/$isduiurl/g" values.yaml
-if [ $? == 0 ];
-then
-   #echo "If linux .."
-   sed -i "s/cd.ryzon7-argo22.opsmx.org/$argocdurl/g" values.yaml
-   #sed -i "s/workflow.ryzon7-argo22.opsmx.org/$argowrkurl/g" values.yaml
-   sed -i "s/rollouts.ryzon7-argo22.opsmx.org/$argoroll/g" values.yaml
-else
-   #echo "If MAC ..."
+if [[ $OSTYPE == 'darwin'* ]]; then
    sed -i.bu "s/oes.example.ops.com/$isduiurl/g" values.yaml
    sed -i.bu "s/cd.ryzon7-argo22.opsmx.org/$argocdurl/g" values.yaml
    #sed -i.bu "s/workflow.ryzon7-argo22.opsmx.org/$argowrkurl/g" values.yaml
    sed -i.bu "s/rollouts.ryzon7-argo22.opsmx.org/$argoroll/g" values.yaml
+else
+   sed -i "s/oes.example.ops.com/$isduiurl/g" values.yaml
+   sed -i "s/cd.ryzon7-argo22.opsmx.org/$argocdurl/g" values.yaml
+   #sed -i "s/workflow.ryzon7-argo22.opsmx.org/$argowrkurl/g" values.yaml
+   sed -i "s/rollouts.ryzon7-argo22.opsmx.org/$argoroll/g" values.yaml
 fi
 
 yq e -i '.cdagentname = "argocd"' values.yaml
