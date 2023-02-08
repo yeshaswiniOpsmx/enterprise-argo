0d00ed9f9a02cb79fb281d86e667d5adb36834e2
maheshopsmx <mahesh.kota@opsmx.io>
2023-02-08 17:31:13 +0530

    updated to install via helm

--- curl/4.1.2/argo-agent.sh
+++ curl/4.1.2/argo-agent.sh
@@ -263,11 +263,11 @@ fi
 echo "Installing..."
 echo ""
 ###########################
-rm -rf enterprise-argo
-git clone https://github.com/OpsMx/enterprise-argo.git > /dev/null 2>&1
-cd enterprise-argo/charts/isdargo
-helm install $argonamespace-isd . -f ../../../values.yaml --namespace $argonamespace
-#helm install isdargo-$argonamespace isdargo/isdargo -f values.yaml --version=$version --namespace $argonamespace
+#rm -rf enterprise-argo
+#git clone https://github.com/OpsMx/enterprise-argo.git > /dev/null 2>&1
+#cd enterprise-argo/charts/isdargo
+#helm install $argonamespace-isd . -f ../../../values.yaml --namespace $argonamespace
+helm install isdargo-$argonamespace isdargo/isdargo -f values.yaml --version=$version --namespace $argonamespace --timeout=15m
 
 ####################
 }
